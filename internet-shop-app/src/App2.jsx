58a4fd88a7f239b406de3814b5844789d93540a2
OPAIQINF <opa200510@gmail.com>
2024-05-18 17:35:25 +1000

    add new page basket

---
+++ internet-shop-app/src/App2.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react'
+import Header from './Header/Header.jsx'
+import PageTab from './PageTab/PageTab.jsx'
+import Footer from './Footer/Footer.jsx'
+
+export default function App2() {
+  
+
+  return (
+    <>
+     <Header/>
+     <PageTab/>
+     <Footer/>
+    </>
+  )
+}
--- internet-shop-app/src/main.jsx
+++ internet-shop-app/src/main.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import App2 from './App2.jsx'
 import './index.css'
-
+import { BrowserRouter,Routes, Route } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/internet-shop" element={<App/>} />
+        <Route path="/internet-shop/basket" element={<App2/>} />
+      </Routes>
+   </BrowserRouter>
+
    </React.StrictMode>
   
 )
