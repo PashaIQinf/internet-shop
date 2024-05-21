19b74cb6fc206360176bbdd80dae7402f6ba3d91
OPAIQINF <opa200510@gmail.com>
2024-05-21 18:47:09 +1000

    fix bug with Product

--- internet-shop-app/src/Product/Product.jsx
+++ internet-shop-app/src/Product/Product.jsx
@@ -198,54 +198,6 @@ export default function Product() {
                 <Button className='Product-buy'><img src={Shoppingcart}/></Button>
             </div>
         </div>
-        <div className='Product'>
-            <img src ={Gleis} />
-            <div className='Product-rating'>
-                <div className='Product-border'>1 баллов</div>
-                <div className='Product-end'>1 Куплено</div>
-            </div>
-            <a className='Product-name' href="/internet-shop/item#Comment">Конфета «Глэйс» с шоколадным вкусом.</a>
-            <div className='Product-cost'>
-                <div>1000 руб. за штуку</div>
-                <Button className='Product-buy'><img src={Shoppingcart}/></Button>
-            </div>
-        </div>
-        <div className='Product'>
-            <img src ={Gleis} />
-            <div className='Product-rating'>
-                <div className='Product-border'>1 баллов</div>
-                <div className='Product-end'>1 Куплено</div>
-            </div>
-            <a className='Product-name' href="/internet-shop/item#Comment">Конфета «Глэйс» с шоколадным вкусом.</a>
-            <div className='Product-cost'>
-                <div>1000 руб. за штуку</div>
-                <Button className='Product-buy'><img src={Shoppingcart}/></Button>
-            </div>
-        </div>
-        <div className='Product'>
-            <img src ={Gleis} />
-            <div className='Product-rating'>
-                <div className='Product-border'>1 баллов</div>
-                <div className='Product-end'>1 Куплено</div>
-            </div>
-            <a className='Product-name' href="/internet-shop/item#Comment">Конфета «Глэйс» с шоколадным вкусом.</a>
-            <div className='Product-cost'>
-                <div>1000 руб. за штуку</div>
-                <Button className='Product-buy'><img src={Shoppingcart}/></Button>
-            </div>
-        </div>
-        <div className='Product'>
-            <img src ={Gleis} />
-            <div className='Product-rating'>
-                <div className='Product-border'>1 баллов</div>
-                <div className='Product-end'>1 Куплено</div>
-            </div>
-            <a className='Product-name' href="/internet-shop/item#Comment">Конфета «Глэйс» с шоколадным вкусом.</a>
-            <div className='Product-cost'>
-                <div>1000 руб. за штуку</div>
-                <Button className='Product-buy'><img src={Shoppingcart}/></Button>
-            </div>
-        </div>
     </div>
   )
 }
